3ce58052eafe05fc2bbf7f3f0c38238690d32b37
StellaShabin <shajistella4@gmail.com>
2024-10-02 09:12:44 +0530

    written test cases

---
+++ test_predict.py
@@ -0,0 +1,24 @@
+from predict import app
+import pytest
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_pinger(client):
+    resp=client.get('/ping')
+    assert resp.status_code == 200
+    assert resp.json == {"Message":"I am pinging.....!!"}
+
+def test_predict(client):
+    test_data={
+    "Gender" : "Male",
+    "Married" : "Unmarried",
+    "ApplicantIncome": 50000,
+    "Credit_History" : "Cleared Debts",
+    "LoanAmount" : 500000
+    }
+    resp=client.post('/predict',json=test_data)
+    assert resp.status_code==200
+    assert resp.json=={"loan_approval_status":"Rejected"}
---
+++ test_square.py
@@ -0,0 +1,5 @@
+from square import get_square
+def test_get_square():
+     get_square(2)==4
+     get_square(5)==25
+test_get_square()
